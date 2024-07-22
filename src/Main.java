86a6447255169d9bcc6ae1bf10745b431ee8ae84
Bunyamin Gokcen <bnymn.gkcn2@gmail.com>
2024-07-22 09:31:02 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        int n = 4;
+
+
+        for (int i = 0; i < n; i++) {
+            for (int j = n - 1; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = n - 1; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
